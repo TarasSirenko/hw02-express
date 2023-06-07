5f02ae01587e36c068f6bbd88439f74b45e3a471
Taras Sirenko <tarassirenko71@gmail.com>
2023-06-07 13:45:26 +0300

    закончил вторую дз, добавил валидацию с помощью библиотеки Joi

---
+++ models/contacts.js
@@ -0,0 +1,93 @@
+const { readFile, writeFile } = require("fs").promises;
+
+
+const path = require("path");
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const data = await readFile(contactsPath, "utf8");
+    const allContacts = JSON.parse(data);
+    const contact = allContacts.find(({ id }) => id === contactId);
+    if (contact) return contact;
+    throw new Error("Contact not found");
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const data = await readFile(contactsPath, "utf8");
+    const allContacts = JSON.parse(data);
+    const contact = allContacts.find(({ id }) => id === contactId);
+
+    if (contact) {
+       const updateContacts = allContacts.filter((contact) => {
+         if (contact.id !== contactId) return contact;
+         return null;
+       });
+      return writeFile(contactsPath, JSON.stringify(updateContacts));
+    }
+    throw new Error("Not found");
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+const addContact = async (body) => {
+   try {
+     const data = await readFile(contactsPath, "utf8");
+     const allContacts = JSON.parse(data);
+
+     const updateContacts = [...allContacts,  body ];
+     return writeFile(contactsPath, JSON.stringify(updateContacts));
+   } catch (err) {
+     console.error(err);
+     throw err;
+   }
+};
+
+const updateContact = async (contactId, body) => {
+   try {
+     const data = await readFile(contactsPath, "utf8");
+     const allContacts = JSON.parse(data);
+     const contactIndex = allContacts.findIndex(({ id }) => id === contactId);
+     if (contactIndex === -1) throw new Error("Contact not found");
+
+    const updatedContacts = allContacts.map((contact) => {
+        if (contact.id === contactId) {
+          return { ...contact, ...body };
+        }
+        return contact;
+      });
+    
+     writeFile(contactsPath, JSON.stringify(updatedContacts));
+     
+     return updatedContacts[contactIndex];
+   } catch (err) {
+     console.error(err);
+     throw err;
+   }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+};
